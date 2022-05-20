369e1d41b44526426b4a7eb22b4e38d38c15ab83
Hao Tran Thien <mr.tranthienhao@gmail.com>
2022-05-20 22:04:25 +0700

    Update images file type

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,19 @@
 class GalleryType {
-  static Prewedding = new GalleryType("prewedding", "Pre-wedding", 49);
+  static Prewedding = new GalleryType("prewedding", "Pre-wedding", 49, 'jpg');
   static MassesAndRites = new GalleryType(
     "massed-and-rites",
     "Thánh lễ & nghi thức",
-    306
+    306,
+    'jpeg'
   );
-  static GroomParty = new GalleryType("groom-party", "Tiệc nhà trai", 224);
-  static BrideParty = new GalleryType("bride-party", "Tiệc nhà gái", 153);
+  static GroomParty = new GalleryType("groom-party", "Tiệc nhà trai", 224,'jpeg');
+  static BrideParty = new GalleryType("bride-party", "Tiệc nhà gái", 153,'jpeg');
 
-  constructor(name, displayName, numberOfImages) {
+  constructor(name, displayName, numberOfImages, fileType) {
     this.name = name;
     this.numberOfImages = numberOfImages;
     this.displayName = displayName;
+    this.fileType = fileType;
   }
 }
 
@@ -44,7 +46,7 @@ class GalleryType {
       imagePath,
       `${galleryType.name}-images%2F${
         isThumb ? "thumb%2F" : ""
-      }${imageIndex}.jpg`
+      }${imageIndex}.${galleryType.fileType}`
     );
   };
 
